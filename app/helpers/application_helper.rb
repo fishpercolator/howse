68e76b801575319183b36a5e757fec3279ce2a74
Rich Daley <rich@fishpercolator.co.uk>
2016-02-23 14:53:48 +0000

    Add dummy areas page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def areas
+    [
+      {name: 'Bramley Falls', rating: 'gold', time: '3 days', location: 'Bramley', pic: 'http://media.rightmove.co.uk/dir/38k/37026/37718175/37026_PUD150636_IMG_00_0000_max_656x437.JPG'},
+      {name: 'Chapeltown Heights', rating: 'gold', time: '14 days', location: '1 mile from Headingley', pic: 'http://media.rightmove.co.uk/dir/60k/59925/52094350/59925_CRE1000112_IMG_01_0000_max_656x437.jpg'},
+      {name: 'Horsforth Paddock', rating: 'bronze', time: '1 year', location: 'Horsforth', pic: 'http://media.rightmove.co.uk/dir/62k/61293/38390022/61293_25956895_IMG_01_0000_max_656x437.jpg'}
+    ]
+  end
+  
 end
