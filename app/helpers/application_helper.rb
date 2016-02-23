3011fb2fd7c804ab350a4bd78bdb6153009b11da
Rich Daley <rich@fishpercolator.co.uk>
2016-02-23 15:53:06 +0000

    Add a 'council mode'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   
   def areas
     [
-      {name: 'Bramley Falls', rating: 'gold', time: '3 days', location: 'Bramley', pic: 'http://media.rightmove.co.uk/dir/38k/37026/37718175/37026_PUD150636_IMG_00_0000_max_656x437.JPG'},
-      {name: 'Chapeltown Heights', rating: 'gold', time: '14 days', location: '1 mile from Headingley', pic: 'http://media.rightmove.co.uk/dir/60k/59925/52094350/59925_CRE1000112_IMG_01_0000_max_656x437.jpg'},
-      {name: 'Horsforth Paddock', rating: 'bronze', time: '1 year', location: 'Horsforth', pic: 'http://media.rightmove.co.uk/dir/62k/61293/38390022/61293_25956895_IMG_01_0000_max_656x437.jpg'}
+      {name: 'Bramley Falls', rating: 'gold', time: '3 days', location: 'Bramley', users: 1345, priority: 'B', pic: 'http://media.rightmove.co.uk/dir/38k/37026/37718175/37026_PUD150636_IMG_00_0000_max_656x437.JPG'},
+      {name: 'Chapeltown Heights', rating: 'gold', time: '14 days', location: '1 mile from Headingley', users: 7766, priority: 'C', pic: 'http://media.rightmove.co.uk/dir/60k/59925/52094350/59925_CRE1000112_IMG_01_0000_max_656x437.jpg'},
+      {name: 'Horsforth Paddock', rating: 'bronze', time: '1 year', location: 'Horsforth', users: 19362, priority: 'A', pic: 'http://media.rightmove.co.uk/dir/62k/61293/38390022/61293_25956895_IMG_01_0000_max_656x437.jpg'}
     ]
   end
   
